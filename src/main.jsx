3a4c65683748f8c3960b2123a3338bd05f5578e4
judaro26 <131112287+judaro26@users.noreply.github.com>
2025-06-10 16:47:15 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css"; // or remove this line if you’re not using Tailwind or custom styles
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
